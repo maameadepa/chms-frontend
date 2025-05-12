68993aa696d2dc994c731cf4faff4c9e0c90afeb
maameadepa <125594445+maameadepa@users.noreply.github.com>
2025-05-12 12:46:07 +0000

    Update hostels.js

--- js/hostels.js
+++ js/hostels.js
@@ -1,7 +1,4 @@
-// API Configuration
-if (typeof API_URL === 'undefined') {
-    const API_URL = 'https://chms-backend-aqow.onrender.com';
-}
+
 
 // DOM Elements
 const roomsGrid = document.getElementById('roomsGrid');
