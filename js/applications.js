95fd3413cac6b8aa988776e357045116db7c0e12
maameadepa <125594445+maameadepa@users.noreply.github.com>
2025-05-12 13:20:19 +0000

    Update applications.js

--- js/applications.js
+++ js/applications.js
@@ -1,5 +1,6 @@
-const API_URL = window.API_URL 
-  || 'https://chms-backend-aqow.onrender.com';
+if (typeof API_URL === 'undefined') {
+    const API_URL = 'https://chms-backend-aqow.onrender.com';
+}
 
 async function fetchApplications() {
     const res = await fetch(`${API_URL}/applications/my-applications`, { credentials: 'include' });
